51796c1a4f935ceb1d9e0f2b854101b526db818a
Sudhanshu4352 <sudhanshu4352@gmail.com>
2026-07-13 00:58:51 +0530

    Initial commit

---
+++ azurerm_public_ip/main.tf
@@ -0,0 +1,10 @@
+
+variable "public_ips" {}
+
+resource "azurerm_public_ip" "pip" {
+  for_each = var.public_ips
+  name                = each.key
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+  allocation_method   = each.value.allocation_method
+}
